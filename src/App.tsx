b8101b349f4ce1732232a20b02284707a8e3c214
Partha Roy <partha.roy@digicert.com>
2025-09-03 12:16:09 +0530

    added some vulnerable packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+  import randomstring from "randomstring";
+
 
 const App = () => {
+
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Test App</h1>
+      <p>Random String - {randomstring.generate()}</p>
     </div>
   );
 };
 
 export default App;
+
